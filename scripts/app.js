ea20265c663c8a3e0fbbee6a73a2051a07b15135
FEvelychko <maxdevelopenko@gmail.com>
2015-09-10 20:10:05 +0300

    starting of data setting

--- scripts/app.js
+++ scripts/app.js
@@ -2,18 +2,21 @@
  * Created by Maksym on 10.09.2015.
  */
 
-    //debugger;
+//debugger;
 
     function DatePicker(){
         this.currentDate = new Date();
         this.data = [];
+        this.days = [];
         this.day = 86400000;
-        this.months = ["Jan"/*"Fab","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"*/];
+        this.months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+        this.weekdays = ["Sun", "Mon", "Tus", "Wen", "Thu", "Fri", "Sat"];
         return this;
     }
     DatePicker.prototype.getMonthDays = function(){
         for(var index = 0; index < this.months.length; index++){
             this.year = this.currentDate.getFullYear();
+            this.month = this.currentDate.getMonth();
             this.startDate = new Date(this.year, index, 1);
             this.endDate = new Date(this.year, index+1, 1);
             this.monthDays = Math.round((this.endDate - this.startDate)/this.day);
@@ -26,7 +29,7 @@
             '<div class="datepicker">'+
             '<div class="head">'+
             '<div class="previous"></div>'+
-            '<div class="monthview">August 2015</div>'+
+            '<div class="yearview">August 2015</div>'+
             '<div class="next"></div>'+
             '</div>'+
             '<div class="body">'+
@@ -37,18 +40,41 @@
         this.newDiv = document.createElement("div");
         this.newDiv.innerHTML = this.template;
         document.body.appendChild(this.newDiv);
-        this.yearDisplay = this.newDiv.querySelector('.monthview');
         return this;
     };
     DatePicker.prototype.setDate = function(){
-        this.yearDisplay.innerHTML = this.year;
-        return this;
+        this.yearDisplay = this.newDiv.querySelector('.yearview');                 /* setting year dating */
+        this.weekdaysDisplay = this.newDiv.querySelector('.calendarhead');         /* setting week days dating */
+        this.daysDisplay = this.newDiv.querySelector('.calendarbody');
+        this.yearDisplay.innerHTML = this.months[this.currentDate.getMonth()] + " " + this.year;
+        this.ul = document.createElement('ul');
+        this.ul.className = "ul";
+        this.weekdaysDisplay.appendChild(this.ul);
+        for(var i=0; i<this.weekdays.length;i++){
+            this.li = document.createElement('li');
+            this.li.className = "li";
+            this.li.innerHTML = this.weekdays[i];
+            this.ul.appendChild(this.li);
+        }
+        this.startDate = new Date(this.year, this.currentDate.getMonth(), 1);
+        this.endDate = new Date(this.year, this.currentDate.getMonth()+1, 1);
+        this.monthDays = Math.round((this.endDate - this.startDate)/this.day);
+        this.ulDays = document.createElement('ul');
+        this.ulDays.className = "ulDays";
+        this.daysDisplay.appendChild(this.ulDays);
+        for(var j=0;j<this.monthDays;j++){
+            this.liDays = document.createElement('li');
+            this.liDays.className = "li";
+            this.liDays.innerHTML = j;
+            this.ulDays.appendChild(this.liDays);
+        }
+        return this.monthDays;
     };
 
     var datePicker = new DatePicker();
     datePicker.getMonthDays();
     datePicker.getTemplate();
-    datePicker.setDate();
+    console.log(datePicker.setDate());
 
     /*DatePicker.prototype.showDatePicker = function(){
      //this.datePicker = document.getElementById('datePicker');
