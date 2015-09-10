d704664f1a3e4145446ba7ba94389d5e2563326a
FEvelychko <maxdevelopenko@gmail.com>
2015-09-10 20:04:21 +0300

    adding datepicker look

--- scripts/app.js
+++ scripts/app.js
@@ -7,34 +7,59 @@
     function DatePicker(){
         this.data = [];
         this.day = 86400000;
-        this.months = ["Jan","Fab","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+        this.months = ["Jan"/*"Fab","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"*/];
     }
     DatePicker.prototype.getMonthDays = function(year){
         for(var index = 0; index < this.months.length; index++){
+            this.year = year;
             this.startDate = new Date(year, index, 1);
             this.endDate = new Date(year, index+1, 1);
             this.monthDays = Math.round((this.endDate - this.startDate)/this.day);
-            this.data[index] = {month: this.monthDays};
-        }
-        return this;
-    };
-    DatePicker.prototype.decodeMonths = function() {
-        for (var index = 0; index < this.months.length; index++) {
-            this.data[index][this.months[index]] = this.data[index].month;
-            delete this.data[index].month;
+            this.data[index] = {year: this.year, month: this.months[index], days: this.monthDays};
         }
         return this.data;
     };
     DatePicker.prototype.showDatePicker = function(){
-
+        var btn = document.getElementById('btn');
+        var datepicker = document.getElementById('datePicker');
+        btn.addEventListener("click", function(){
+            var newDiv = document.createElement("div");
+            newDiv.className = "newDiv";
+            var leftButton = document.createElement("div");
+            leftButton.className = "leftBtn";
+            leftButton.innerHTML = "left";
+            var yearBlock = document.createElement("div");
+            yearBlock.className = "yearBlock";
+            yearBlock.innerHTML = "year!!!!!!!!";
+            var rightButton = document.createElement("div");
+            rightButton.className = "rightBtn";
+            rightButton.innerHTML = "right";
+            var daysBlock = document.createElement("div");
+            daysBlock.className = "daysBlock";
+            daysBlock.innerHTML = "days!!!!!!!!";
+            datepicker.appendChild(newDiv);
+            newDiv.appendChild(leftButton);
+            newDiv.appendChild(yearBlock);
+            newDiv.appendChild(rightButton);
+            newDiv.appendChild(daysBlock);
+        })
     };
 
     var datePicker = new DatePicker();
     var decodedData = datePicker.getMonthDays(2015);
-    var showDecodedMonthId = datePicker.decodeMonths();
-    console.log(showDecodedMonthId);
-    var shownBlock = datePicker.showDatePicker();
+    console.log(decodedData);
+    datePicker.showDatePicker();
 
+
+
+    /*DatePicker.prototype.decodeMonths = function() {
+     for (var index = 0; index < this.months.length; index++) {
+     //this.data[index][this.months[index]] = this.data[index].month;
+     //this.data[index][this.months[index]] = this.data[index].month;
+     //delete this.data[index].month;
+     }
+     return this.data;
+     };*/
 
     /*DatePicker.prototype.renameProperty = function (oldName, newName) {
      datePicker.decodeMonthId();
