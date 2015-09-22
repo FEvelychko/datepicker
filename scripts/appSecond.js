452199c727768645e3a495a3778d77a23ea4dcb3
FEvelychko <maxdevelopenko@gmail.com>
2015-09-22 12:28:04 +0300

    redesign and feature of getting future data

--- scripts/appSecond.js
+++ scripts/appSecond.js
@@ -1,11 +1,12 @@
 /**
  * Created by Maksym on 21.09.2015.
  */
-    var day,ul,year,startCurrentMonthDays,startNextMonthDays,months,btnPrev,startDate,endDate,firstMonth,secondMonth,weekdays,monthDays,prevDate,prevMonthDays,showingDate;
+    var day,li,ulList,liQuantity,year,yearDisplay,newDiv,startCurrentMonthDays,startNextMonthDays,months,btnPrev,btnNext,startDate,endDate,firstMonth,secondMonth,weekdays,monthDays,prevDate,prevMonthDays,showingDate;
     year = 2015;
     day = 86400000;
     firstMonth = 3;
     secondMonth = 4;
+    liQuantity = 49;
     startNextMonthDays = 0;
     startCurrentMonthDays = 1;
     prevDate = new Date(year, firstMonth-1, 1);
@@ -13,58 +14,89 @@
     endDate = new Date(year, secondMonth, 1);
     monthDays = Math.round((endDate - startDate) / day);
     prevMonthDays = Math.round((startDate - prevDate) / day);
-    btnPrev = document.getElementById('prev');
-    ul = document.getElementById('ul').children;
+    newDiv = document.createElement("div");
     showingDate = document.getElementById('date');
     weekdays = ["Sun", "Mon", "Tus", "Wen", "Thu", "Fri", "Sat"];
     months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-    showingDate.innerHTML = monthDays + " " + months[firstMonth] + " " + year;
+
+    function addTemplate(){
+
+        var template =
+            '<div class="datepicker">'+
+            '<div class="headinput"></div>'+
+            '<div class="head">'+
+            '<div class="previous"><button class = "btn" id = "prev">prev</button></div>'+
+            '<div class="yearview">August 2015</div>'+
+            '<div class="next"><button class = "btn" id = "next">next</button></div>'+
+            '</div>'+
+            '<div class="body">'+
+            '<div class="calendarbody">' +
+            '<ul id = "ul" class = "ul"></ul>' + '</div>'+ '</div>'+
+            '</div>';
+        newDiv.innerHTML = template;
+        document.body.appendChild(newDiv);
+        ulList = document.getElementById('ul');
+        for(var x = 0; x < liQuantity; x++){
+            li = document.createElement('li');
+            ulList.appendChild(li);
+            ulList.children[x].className = 'li';
+        }
+        yearDisplay = newDiv.querySelector('.yearview');
+        yearDisplay.innerHTML = monthDays + " " + months[firstMonth] + " " + year;
+
+    };
 
     function fillDays(){
-        for(var x = 0; x < ul.length; x++){
-            ul[x].className = 'liSecond';
+
+        btnNext = document.getElementById('next');
+        btnPrev = document.getElementById('prev');
+        ulList = document.getElementById('ul').children;
+        for(var x = 0; x < ulList.length; x++){
             var firstDay = 7 + startDate.getDay();
             if(x < 7){
-                ul[x].innerHTML = weekdays[x];
+                ulList[x].innerHTML = weekdays[x];
             }
             if(x >= 7 && x < firstDay){
-                ul[x].innerHTML = lastDays(startDate.getDay());
+                ulList[x].innerHTML = lastDays(startDate.getDay());
             }
             if(x >= firstDay && x < firstDay+1){
-                ul[x].innerHTML = startCurrentMonthDays;
+                ulList[x].innerHTML = startCurrentMonthDays;
             }
             if(x >= firstDay+1 && x < firstDay+monthDays){
-                ul[x].innerHTML = incrementCurrentMonthDays();
+                ulList[x].innerHTML = incrementCurrentMonthDays();
             }
-            if(x >= firstDay+monthDays && x < ul.length){
-                ul[x].innerHTML = incrementNextMonthDays();
+            if(x >= firstDay+monthDays && x < ulList.length){
+                ulList[x].innerHTML = incrementNextMonthDays();
             }
         }
-    }
+    };
 
     function update(){
         startCurrentMonthDays = 1;
         startNextMonthDays = 0;
-    }
+    };
 
     function lastDays(value){
         prevMonthDays += 1;
         return prevMonthDays - value;
-    }
+    };
 
     function incrementCurrentMonthDays(){
         return startCurrentMonthDays += 1;
-    }
+    };
 
     function incrementNextMonthDays(){
         return startNextMonthDays += 1;
-    }
+    };
 
     function setEvents(){
-        btnPrev.addEventListener('click', function(){
+        btnNext.addEventListener('click', function(){
             getNextData(update, fillDays)
         }, false);
-    }
+        btnPrev.addEventListener('click', function(){
+            getPrevData(update, fillDays)
+        }, false);
+    };
 
     function getNextData(callUpdate, callFillDays){
         firstMonth += 1;
@@ -74,18 +106,34 @@
         endDate = new Date(year, secondMonth, 1);
         monthDays = Math.round((endDate - startDate) / day);
         prevMonthDays = Math.round((startDate - prevDate) / day);
-        showingDate.innerHTML = monthDays + " " + months[firstMonth] + " " + year;
+        yearDisplay.innerHTML = monthDays + " " + months[firstMonth] + " " + year;
         callUpdate();
         callFillDays();
-    }
+    };
 
+    function getPrevData(callUpdate, callFillDays){
+        firstMonth -= 1;
+        secondMonth -= 1;
+        prevDate = new Date(year, firstMonth-1, 1);
+        startDate = new Date(year, firstMonth, 1);
+        endDate = new Date(year, secondMonth, 1);
+        monthDays = Math.round((endDate - startDate) / day);
+        prevMonthDays = Math.round((startDate - prevDate) / day);
+        yearDisplay.innerHTML = monthDays + " " + months[firstMonth] + " " + year;
+        callUpdate();
+        callFillDays();
+    };
+
+    addTemplate();
     fillDays();
     setEvents();
 
     console.log(weekdays[startDate.getDay()]);
     console.log(weekdays[endDate.getDay()]);
     console.log(monthDays);
-    console.log(ul.length);
+
+
+
 
 
 
